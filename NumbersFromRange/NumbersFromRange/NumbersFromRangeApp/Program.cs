257fb7142f612b9408ca4ce01a3c122daacaf4b7
Vkube <45425011+Vkube@users.noreply.github.com>
2022-11-17 18:13:28 +0300

    numbersfromrange

---
+++ NumbersFromRange/NumbersFromRange/NumbersFromRangeApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumbersFromRangeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new[] { 67, 92, 153, 15 };
+            var solution = NumberFromRange(array);
+        }
+
+        public static int[] NumberFromRange(int[] array)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 30 && array[i] < 100)
+                {
+                    numbers.Add(array[i]);
+                }
+            }
+
+            return numbers.ToArray();
+        }
+    }
+}
---
+++ NumbersFromRange/Task.md
@@ -0,0 +1,8 @@
+
+<h2>Numbers from range<h2>
+<p>Given an array of integers, write a query that returns list of numbers greater than 30 and less than 100.<p>
+
+```c#
+Expected input and output
+[67, 92, 153, 15] → 67, 92
+```
