bf3f33d4800c30e1d33bcb11653a810a8fd10726
Vkube <45425011+Vkube@users.noreply.github.com>
2022-11-17 18:13:12 +0300

    minimum length

---
+++ MinimumLength/MinimumLength/MinimumLengthApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinimumLengthApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animals = new List<string> { "zebra", "elephant", "cat", "dog", "rhino", "bat" };
+            MinimumLength(animals);
+        }
+
+        private static List<string> MinimumLength(List<string> words)
+        {
+            var animals = words.Where(word => word.Length >= 5).Select(word => word.ToUpper());
+            return animals.ToList();
+        }
+    }
+}
---
+++ MinimumLength/Task.md
@@ -0,0 +1,6 @@
+<h2>Minimum length<h2>
+<p>Write a query that returns words at least 5 characters long and make them uppercase.</p>
+
+```c#
+"computer", "usb" → "COMPUTER"
+```
