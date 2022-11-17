d14285aa3d47a23801fedc01b6034e26c1b33cfd
Vkube <45425011+Vkube@users.noreply.github.com>
2022-11-17 18:13:38 +0300

    selectwords

---
+++ SelectWords/SelectWords/SelectWordsAp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelectWordsAp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SelectedWord
+            var words = new List<string>() { "mum", "amsterdam", "bloom" };
+            var selectedWord = words.Where(word => word.StartsWith("a") && word.EndsWith("m"));
+        }
+    }
+}
---
+++ SelectWords/Task.md
@@ -0,0 +1,8 @@
+<h2>Select Words</h2>
+
+<p>Write a query that returns words starting with letter 'a' and ending with letter 'm'.</p>
+
+```c#
+Expected input and output
+"mum", "amsterdam", "bloom" → "amsterdam"
+```
